ca9ce665462e3992649b1f9d7c08fdc3b99ec553
nickhetter <nhetter@ucsc.edu>
2024-11-21 22:05:58 -0800

    done

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,37 @@
 // index.js - purpose and description here
-// Author: Your Name
+// Author: Nicholas Hetter 
 // Date:
 
-// Constants
+// Rock-paper-scissors (best two out of three)
+// Yi Ho&Nicholas Hetter
+// October 16 2024
+//Print the explanation of the “best two out of thee” rule.
+//Set “wins” counters to both the player and the computer
+  //Print choices (rock, paper, and scissors) 
+		//Validate the choice of the player
+	  //Store the choice of the player
+	//Randomize a choice for the computer (the possibilities should all be 1/3)
+	//Counting down from 3 and publish the answers
+  //Test the results and decide the winner, print “draw” if draw.
+	//Add 1 to the winner’s “wins” counter if there is a winner
+//If the counter of one of them reach 3 (3 wins), print “the winner of this game is” and the winner’s name (user or computer)
+//If there is still no winner, repeat the previous steps
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//How to change a tire. 
+//First, make sure you are in a safe area away from any oncoming traffic.  
+//Walk around the car to see which tire is flat. Find the flat tire.
+  //Once located, open car trunk and find replacement tire, jack, and tool kit.
+    //Go to the damaged tire and locate adequate jacking points, placing the spare wheel to the side for future use.
+      //Place the jack under the car vertically with the feet facing the bottom and the support facing up.
+      //Loose the lugnuts counterclockwise on the tires with the provided wrench in the tool kit. 
+      //Crank the jack until the tire is raised 2 inches off of the ground. 
+      //Loosen all of the lugnuts and remove them, saving them in a safe place. 
+      //Remove the wheel, slide it off of the bolt-threaded studs and place it in the trunk of the car. 
+      //Now grab the spare tire and align the bolt’s threaded studs with the boltholes on the wheel.
+      //Once aligned, place lugnuts on the bolt threads and tighten in a star pattern placing each lug nut on the thread and tightening clockwise with the tire wrench.
+      //Tighten lug nuts as tight as possible. 
+      //On the jack, there should be a small knob that you can twist to release pressure from the jack.
+      //Remove jack.
+    //Twist this knob counterclockwise slowly to lower the car onto the replacement tire. 
+//Done! You’re ready to drive. 
